a945e5e3d4eb205226001831120473bdf7830156
dannyfrelink <danny-frelink@hotmail.com>
2022-02-21 18:19:48 +0100

    add animation to home page

---
+++ scripts/animation.js
@@ -0,0 +1,20 @@
+const faders = document.querySelectorAll('main>*');
+
+const appearOptions = {
+    threshold: 0.8,
+}
+
+const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.add('appear');
+            appearOnScroll.unobserve(entry.target);
+        }
+    })
+}, appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+})
